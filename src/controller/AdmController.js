04db9e67ad4677beef59dc8a496ed091b4dd3660
Shockwaiver <38588489+Shockwaiver@users.noreply.github.com>
2020-11-24 01:56:07 -0300

    AdmController fix

--- src/controller/AdmController.js
+++ src/controller/AdmController.js
@@ -6,7 +6,7 @@ module.exports = {
 
     async curriculos(req,res){
 
-        const curriculos = await Anuncio.findAll();
+        const curriculos = await Anuncio.findAll({where: {categoria:"curriculo"}});
 
         if(!curriculos)
         return res.status(400).json({error:"Nenhum curriculo registrado"});
@@ -18,7 +18,7 @@ module.exports = {
 
     async usuarios(req,res){
 
-        const usuarios = await Usuario.findAll({where: {empresa:""}});
+        const usuarios = await Usuario.findAll({where: {empresa:" "}});
 
         if(!usuarios)
         return res.status(400).json({error:"Nenhum usuario registrado"});
@@ -30,7 +30,7 @@ module.exports = {
 
     async empresas(req,res){
  	
-        const empresas = await Usuario.findAll({where: {empresa: { [Op.not]:""}}});
+        const empresas = await Usuario.findAll({where: {empresa: { [Op.not]:" "}}});
 
         if(!empresas)
         return res.status(400).json({error:"Nenhuma empresa registrada"});
@@ -42,7 +42,7 @@ module.exports = {
 
 	async vagas(req,res){
 
-        const vagas = await Anuncio.findAll();
+        const vagas = await Anuncio.findAll({where: {categoria:"vaga"}});
 
         if(!vagas)
         return res.status(400).json({error:"Nenhuma empresas registrada"});
